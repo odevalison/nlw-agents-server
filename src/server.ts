94b20d4ae838f2f7b0020b781d4ac5c65268450c
Alison G. <odevalison@gmail.com>
2025-07-09 16:00:32 -0300

    feat: add create question, create room and get room questions

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,10 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { sql } from './db/connection.ts'
 import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -23,5 +25,8 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestionsRoute)
+app.register(createQuestionRoute)
 
 app.listen({ port: env.PORT })
